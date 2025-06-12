280eb056a32f2883a18a5786f1d8fe8e0c25214b
desabi <desabi@live.com>
2025-06-11 19:00:38 -0600

    feat: sb-demo-a, base project

---
+++ camel-sb-demo-a/src/main/java/com/desabi/guide/camel/spring/CamelSbDemoAApplication.java
@@ -0,0 +1,13 @@
+package com.desabi.guide.camel.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelSbDemoAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelSbDemoAApplication.class, args);
+	}
+
+}
---
+++ camel-sb-demo-a/src/test/java/com/desabi/guide/camel/spring/CamelSbDemoAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desabi.guide.camel.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelSbDemoAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
