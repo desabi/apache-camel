4645afb23cf75bb5d30bdeeb6dfec46ba44ae2e7
desabi <desabi@live.com>
2025-06-13 09:44:04 -0600

    feat: soap-server, base project

---
+++ camel-soap-server/src/main/java/com/desabi/guide/camel/soap/CamelSoapServerApplication.java
@@ -0,0 +1,13 @@
+package com.desabi.guide.camel.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelSoapServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelSoapServerApplication.class, args);
+	}
+
+}
---
+++ camel-soap-server/src/test/java/com/desabi/guide/camel/soap/CamelSoapServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desabi.guide.camel.soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelSoapServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
