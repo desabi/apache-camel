65eb015a4b2c8515821dfba117f930cf7f97ac0c
desabi <desabi@live.com>
2025-06-13 18:50:19 -0600

    feat: soap-cliente, base project

---
+++ camel-soap-client/src/test/java/com/desabi/guide/camel/soap/client/CamelSoapClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desabi.guide.camel.soap.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelSoapClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
