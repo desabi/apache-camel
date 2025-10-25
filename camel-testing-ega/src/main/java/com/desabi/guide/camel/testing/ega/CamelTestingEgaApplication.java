2f5918590c7974bde848e9b5ca0aab3c460376e3
desabi <desabi@live.com>
2025-10-25 08:36:29 -0600

    feat: testing-ega, base project

---
+++ camel-testing-ega/src/main/java/com/desabi/guide/camel/testing/ega/CamelTestingEgaApplication.java
@@ -0,0 +1,13 @@
+package com.desabi.guide.camel.testing.ega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelTestingEgaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelTestingEgaApplication.class, args);
+	}
+
+}
---
+++ camel-testing-ega/src/test/java/com/desabi/guide/camel/testing/ega/CamelTestingEgaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desabi.guide.camel.testing.ega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelTestingEgaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
